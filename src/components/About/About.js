95493e64cb2afa5ac3e15719c445df2f2d8ed21d
kingofassguard <psp09@my.yorku.ca>
2022-08-18 00:29:32 -0400

    Changes to about!
    Added a profile image to about section and added a bio!

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './AboutStyles';
+import { LeftSection, Img, Divi} from './AboutStyles';
 
 const About = () => (
 <Section id="about">
     <SectionDivider />
     <br />
     <br />
-    
+
     <SectionTitle>About</SectionTitle>
+    <Divi>
+        <Img src="images/dp.png" />
+    </Divi>
+
     <LeftSection>
         <SectionText>
-            Hi my name is Priyanshu!
+            My name is Priyanshu and I am a motivated BSc. Computer Science graduate from York University 
+            that comes with a wide array of industry applicable skillset. At the moment, I am pursuing a career
+            in backend development while strengthening my grasp of frontend to become an elite full-stack!
+            Currently working on new projects to show off my skills! 😎
+            <br />
+            Other than coding, I enjoy mountain biking🚵, exploring🌵 and playing video games🎮!    
         </SectionText>
     </LeftSection>
 </Section>
--- src/components/About/AboutStyles.js
+++ src/components/About/AboutStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const LeftSection = styled.div`
-  width: 100%;
+  width: 69%;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 80%;
     display: flex;
@@ -16,4 +16,30 @@ export const LeftSection = styled.div`
 
     margin: 0 auto;
   }
+`
+export const Img = styled.img`
+    width: 300px;
+    height: 250px;
+    position: relative;
+    top: -24px;
+    left: -30px;
+    object-fit: scale-down;
+`
+
+export const Divi = styled.div`
+    position: absolute;
+    right : 40px;
+    top: 185px;
+    width: 280px;
+    height: 250px;
+    background: #1388c7;
+    margin: auto;
+    padding: 15px 10px;
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        visibility: hidden;
+      }
+      @media ${(props) => props.theme.breakpoints.md} {
+        visibility: hidden;
+      }
 `;
