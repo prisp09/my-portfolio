5e2d3bf2a9c569760b4477bc89fc950db2ce5e09
kingofassguard <psp09@my.yorku.ca>
2022-10-28 23:53:06 -0400

    Improved Title
    Added a better title and added a new rounded favicon.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,15 @@ import About from '../components/About/About';
 import Layout from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 import UpButton from '../components/UpButton/UpButton';
-
+import Head from 'next/head';
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Priyanshu's Portforlio</title>
+        <meta name="Priyanshu Patel's Portforlio." content="This page contains the portfolio of Priyanshu Patel, highlighting his skills and projects." />
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
