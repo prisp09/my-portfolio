69e3802b5f495287ee23bab348244e7fcf21f85c
Priyanshu Patel <priyanshu.sanjay.patel@gmail.com>
2025-03-16 06:40:56 -0400

    folder changes

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div>
+        <span className="bg-yellow-500 text-black font-sans">TODO:</span> ALOT
+      </div>
+    </div>
+  );
+}
